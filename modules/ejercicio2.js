d579d8f7bbbda807f0234689c62cce2c419fe9a5
michaelbenavides <michaelfb803@gmail.com>
2026-03-04 11:43:21 -0500

    envio avance antes de que se acabe la clase. avance del ejercico2,

--- modules/ejercicio2.js
+++ modules/ejercicio2.js
@@ -1,23 +1,52 @@
 export const ejercicio2 = () => {
     console.log("Ejecutando el ejercicio 2");
+    // 2. Fusión de catálogos digitales
+    // Requerimiento:
+    // Tienes dos catálogos:
+    // const catalogoA = [
+    // { id: 1, nombre: "Curso JavaScript", precio: 40 },
+    // { id: 2, nombre: "Curso HTML", precio: 35 }
+    // ];
+    // const catalogoB = [
+    // { id: 3, nombre: "Curso CSS", precio: 30 }
+    // ];
+    
+    // Crea una función fusionarCatalogos(a, b) que:
+    
+    // 1. Use try...catch para validar que ambos parámetros sean arreglos.
+    // 2. Utilice spread para fusionarlos sin modificar los originales.
+    // 3. Retorne un nuevo catálogo ordenado por precio ascendente.
+    // Resultado esperado:
+    // Una nueva colección combinada, ordenada e inmutable. Si un parámetro no es un arreglo,
+  // debe mostrarse un error comprensible.
+  
+  //creamos el primer arregglo que se llama catalogoA, y contiene 2 objetos.
+  const catalogoA = [{
+    id: 1,
+    nombre: "curso JavaScript",
+    precio: 40
+  },
+  {
+    id: 2,
+    nombre: "curso HTML",
+    precio: 35
+  }]
+  //creamos el segundo arreglo que se llama catalogoB, y contiene un solo objeto
+  const catalogoB = [{
+    id: 3,
+    nombre: "curso CSS",
+    precio : 30
+  }]
+
+  //creamos la funciona para validad por medio de try catch, que ambos parametro que recibe sean arreglos
+
+  function validarArreglos(a, b) {
+    
+    try
+
+  }
+
+
+
+
 }
-
-// 2. Fusión de catálogos digitales
-// Requerimiento:
-// Tienes dos catálogos:
-// const catalogoA = [
-// { id: 1, nombre: "Curso JavaScript", precio: 40 },
-// { id: 2, nombre: "Curso HTML", precio: 35 }
-// ];
-// const catalogoB = [
-// { id: 3, nombre: "Curso CSS", precio: 30 }
-// ];
-
-// Crea una función fusionarCatalogos(a, b) que:
-
-// 1. Use try...catch para validar que ambos parámetros sean arreglos.
-// 2. Utilice spread para fusionarlos sin modificar los originales.
-// 3. Retorne un nuevo catálogo ordenado por precio ascendente.
-// Resultado esperado:
-// Una nueva colección combinada, ordenada e inmutable. Si un parámetro no es un arreglo,
-// debe mostrarse un error comprensible.
